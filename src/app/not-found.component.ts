f1deed488a5579f382af0f646022a81f0093c39e
Elad Cohen <eladc.android@gmail.com>
2017-12-16 22:34:48 +0200

    deleted files

--- src/app/not-found.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  template: '<h1>Page not found</h1>',
-  styles: [`h1 { font-family: Lato; }`]
-})
-export class PageNotFoundComponent {}
